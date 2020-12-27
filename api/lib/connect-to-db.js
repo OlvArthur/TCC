70a0e31905aa5c2d1b748a0564066dd6e462698b
Arthur Oliveira <olvarthur@gmail.com>
2020-12-27 13:22:19 -0300

    Testing db connection uri

--- api/lib/connect-to-db.js
+++ api/lib/connect-to-db.js
@@ -1,17 +1,24 @@
-const MongoClient = require('mongodb').MongoClient
+const { MongoClient } = require('mongodb')
 
 let cachedDb = null
 
-const uri = process.env.MONGODB_URI
+// const uri = process.env.MONGODB_URI
+const uri = 'mongodb+srv://Deploy:Luanny2509!@cluster0.dsehz.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true'
 console.log(uri)
+
+const client = new MongoClient(uri)
 
 module.exports = async () => {
   if (cachedDb) return cachedDb
 
-  console.log('tentando conectar', cachedDb)
-  const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  await client.connect()
 
-  const db = await client.db(new URL(uri).pathname.substr(1))
+  console.log('tentando conectar', cachedDb)
+  // const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  await client.db('admin').command({ ping: 1 })
+  console.log('Connected successfully to server')
+  const db = client.db(new URL(uri).pathname.substr(1))
 
   cachedDb = db
 
