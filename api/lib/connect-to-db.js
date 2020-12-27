f85f079a59e41894fa4046ffbb7bf05ca879272f
Arthur Oliveira <olvarthur@gmail.com>
2020-12-27 14:03:28 -0300

    Testing db connection uri

--- api/lib/connect-to-db.js
+++ api/lib/connect-to-db.js
@@ -4,23 +4,30 @@ module.exports = async () => {
   let cachedDb = null
 
   // const uri = process.env.MONGODB_URI
-  const uri = 'mongodb+srv://Deploy:Luanny2509!@cluster0.dsehz.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true'
+  // const uri = 'mongodb+srv://Deploy:Luanny2509!@cluster0.dsehz.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true'
+  const uri = 'mongodb+srv://Deploy:Luanny2509!@cluster0.dsehz.mongodb.net/test'
+
   console.log(uri)
 
   const client = new MongoClient(uri)
   console.log('chegou na conexão')
-  console.log('tentando conectar', cachedDb)
-  if (cachedDb) return cachedDb
 
-  await client.connect()
+  try {
+    if (cachedDb) return cachedDb
+    await client.connect()
+    console.log('tentando conectar', cachedDb)
+
+    await client.db('admin').command({ ping: 1 })
+
+    const db = client.db(new URL(uri).pathname.substr(1))
+    console.log('Connected successfully to server')
+
+    cachedDb = db
+
+    return db
+  } catch (err) {
+    console.log(err.stack)
+  }
 
   // const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-
-  await client.db('admin').command({ ping: 1 })
-  console.log('Connected successfully to server')
-  const db = client.db(new URL(uri).pathname.substr(1))
-
-  cachedDb = db
-
-  return db
 }
